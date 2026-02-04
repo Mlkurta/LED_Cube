a3f0fbfc1261a9dc49ecf011b04f41a7a798d7fa
Mlkurta <78199728+Mlkurta@users.noreply.github.com>
2026-02-04 16:38:43 -0700

    Update example.h

--- example.h
+++ example.h
@@ -16,10 +16,23 @@ void example(){
   for(i=0;i<8;i++)
   for(j=0;j<8;j++)
   for(k=0;k<8;k++){
+    // void LED(int16_t level, int16_t row, int16_t column, uint8_t red, uint8_t green, uint8_t blue);
+    // Enter values 0 - 7 for level, row, and column.  Enter values (brightness --> higher = more from 0 - 15) for red, green, and blue.
     LED(i, j, k, 8-i, 8-j, k);
     delay(250);
   }
+
+  // Does the exact same thing excepts it turns off the LEDs in the same order, but turns them off faster than they were turned on (less delay)
+  for(i=0;i<8;i++)
+  for(j=0;j<8;j++)
+  for(k=0;k<8;k++){
+    // void LED(int16_t level, int16_t row, int16_t column, uint8_t red, uint8_t green, uint8_t blue);
+    // Enter values 0 - 7 for level, row, and column.  Enter values (brightness --> higher = more from 0 - 15) for red, green, and blue.
+    LED(i, j, k, 8-i, 8-j, k);
+    delay(50);
+  
 }
+
 
 
 
