c0496d1ecdc0a4ba5c555c5c12fefec4f64d92a4
Mlkurta <78199728+Mlkurta@users.noreply.github.com>
2026-02-04 16:35:11 -0700

    Update example.h

--- example.h
+++ example.h
@@ -2,11 +2,17 @@
 
 #include "LED.h"
 
+/////////////////////////////////
+/////Example LED Cube Sketch///// 
+/////////////////////////////////
 
-void example();
 
 void example(){
   int i, j, k;
+
+  // Turns on LEDs in a doubly nested for loop; Within every verticl level, it increments every row to 8 rows, and within every row, it incrments every column to 8 columns (0 - 7)
+  // Also changes the colors of the LED depending on the level / row / column.  Uses a delay of 250 ms.  
+  // **Note** if you want to turn LEDs off, you must write a zero value to the red, green and blue brightness values
   for(i=0;i<8;i++)
   for(j=0;j<8;j++)
   for(k=0;k<8;k++){
@@ -14,6 +20,7 @@ void example(){
     delay(250);
   }
 }
+
 
 
 
